75a8382494877ee3cf8c0732bed3f8510eb40acf
asmaa oualidi <oualidiasmaa65@gmail.com>
2024-11-08 19:33:42 +0100

    data analyst

---
+++ ADA/file.py
@@ -0,0 +1,17 @@
+ 
+from src.classe import class1
+
+#s2=class1()
+#s2.nom="asmaa"
+#s2.prenom="oualidi"
+#s2.age="23"
+
+#s2.afficher
+
+s3=class1()
+s3.nom="ll"
+s3.prenom="hgf"
+s3.age="14"
+
+s3.nom="kkk"
+s3.afficher()
